f92bc828a04099c53948e0816669705c1ef223b4
AndyAndWoodyForever <2101335186@qq.com>
2026-07-13 11:55:10 +0800

    Complete MaxArrayDeque and its test

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,4 +1,42 @@
 package deque;
 
-public class MaxArrayDeque {
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T>{
+    private Comparator<T> maxComparator;
+
+    /** creates a MaxArrayDeque with the given Comparator */
+    public MaxArrayDeque(Comparator<T> c){
+        super();
+        this.maxComparator = c;
+    }
+
+
+    /** returns the maximum element in the deque as governed by the previously given Comparator.
+     *     If the MaxArrayDeque is empty, simply return null
+      */
+    public T max(){
+        return max(maxComparator);
+    }
+
+    /**  returns the maximum element in the deque as governed by the parameter Comparator c.
+     *     If the MaxArrayDeque is empty, simply return null
+     */
+    public T max(Comparator<T> c){
+        if(isEmpty()){
+            return null;
+        }else{
+            T maxItem = this.get(0);
+
+            for (T item : this) {
+                if (c.compare(item, maxItem) > 0) {
+                    maxItem = item;
+                }
+            }
+            return maxItem;
+
+        }
+    }
+
 }
--- proj1/deque/MaxArrayDequeTest.java
+++ proj1/deque/MaxArrayDequeTest.java
@@ -1,4 +1,72 @@
 package deque;
 
+import org.junit.Test;
+import java.util.Comparator;
+import static org.junit.Assert.assertEquals;
+
+
 public class MaxArrayDequeTest {
+    public class StringLengthComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            // 返回正数说明 a 比 b 长，返回负数说明 a 比 b 短
+            return a.length() - b.length();
+        }
+    }
+    public class StringAlphaComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            return a.compareTo(b);
+        }
+    }
+
+
+    @Test
+    public void lengthtest() {
+        Comparator<String> lenCmp = new StringLengthComparator();
+
+        MaxArrayDeque<String> mad = new MaxArrayDeque<>(lenCmp);
+
+        mad.addLast("dog");
+        mad.addLast("watermelon");
+        mad.addLast("apple");
+        mad.addLast("zebra");
+
+
+        String maxLen = mad.max();
+        assertEquals(maxLen, "watermelon");
+
+    }
+
+    @Test
+    public void alphatest() {
+        Comparator<String> alphaCmp = new StringAlphaComparator();
+
+        MaxArrayDeque<String> mad = new MaxArrayDeque<>(alphaCmp);
+
+        mad.addLast("dog");
+        mad.addLast("watermelon");
+        mad.addLast("apple");
+        mad.addLast("zebra");
+
+        String maxAlpha = mad.max(alphaCmp);
+        assertEquals(maxAlpha, "zebra");
+    }
+
+
+    @Test
+    public void mixtest() {
+        Comparator<String> lenCmp = new StringLengthComparator();
+        Comparator<String> alphaCmp = new StringAlphaComparator();
+
+        MaxArrayDeque<String> mad = new MaxArrayDeque<>(lenCmp);
+
+        mad.addLast("dog");
+        mad.addLast("watermelon");
+        mad.addLast("apple");
+        mad.addLast("zebra");
+
+        String maxAlpha = mad.max(alphaCmp);
+        assertEquals(maxAlpha, "zebra");
+    }
 }
