ea8af28b3156704af9817bdf6d56498f1de6be7a
AndyAndWoodyForever <2101335186@qq.com>
2026-07-22 16:10:51 +0800

    Complete Stage

--- proj2/gitlet/Stage.java
+++ proj2/gitlet/Stage.java
@@ -1,4 +1,23 @@
 package gitlet;
 
-public class Stage {
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Stage implements Serializable {
+    private HashMap<String, String> addition;
+    private HashSet<String> removal;
+
+    public Stage() {
+        this.addition = new HashMap<>();
+        this.removal = new HashSet<>();
+    }
+    public HashMap<String, String> getAddition() {
+        return addition;
+    }
+
+    public HashSet<String> getRemoval() {
+        return removal;
+    }
+
 }
